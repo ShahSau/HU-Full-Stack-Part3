de9b48f25e9569695e4366cb19a48d6c8df5c578
ShahSau <shahriarksaurov@gmail.com>
2021-09-14 01:07:08 +0300

    3.17*: Phonebook database, step5

--- index.js
+++ index.js
@@ -84,6 +84,26 @@ app.post('/api/persons', (req, res) => {
   })
 });
 
+app.put("/api/persons/:id", (req,res,next)=>{
+  const body = req.body;
+  const person ={
+    name: body.name,
+    number: body.number,
+  }
+  
+  // const opts= {
+  //   runValidators: true
+  // }
+  Person.findByIdAndUpdate(req.params.id, person, {new:true,runValidators: true,context: 'query'})
+  .then((updatedPerson) =>{
+    res.json(updatedPerson)
+  })
+  .catch((error)=>{
+    next(error)
+  })
+})
+
+
 morgan.token("person", (req, res) => {
   if (req.method === "POST") {
     return JSON.stringify(req.body);
